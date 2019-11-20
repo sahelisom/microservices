0c053a2173d1ac4da381e4d231d63fef16e9cac7
sahelisom <57664811+sahelisom@users.noreply.github.com>
2019-11-20 16:59:39 +0530

    Add files via upload

---
+++ eureka-naming-server/src/test/java/com/payroll/microservices/eurekanamingserver/EurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payroll.microservices.eurekanamingserver;
+
+import org.junit.*;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
