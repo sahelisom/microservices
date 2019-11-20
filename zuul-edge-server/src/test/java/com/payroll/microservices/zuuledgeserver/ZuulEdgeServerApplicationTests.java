6eb94dab905e4f14604d84cf07e3cdff3880d89f
sahelisom <57664811+sahelisom@users.noreply.github.com>
2019-11-20 17:00:49 +0530

    Add files via upload

---
+++ zuul-edge-server/src/test/java/com/payroll/microservices/zuuledgeserver/ZuulEdgeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payroll.microservices.zuuledgeserver;
+
+import org.junit.*;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulEdgeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
