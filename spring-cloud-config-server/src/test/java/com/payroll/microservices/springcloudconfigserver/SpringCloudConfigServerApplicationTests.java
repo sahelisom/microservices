89bf844ca99731bce06ab5cd3f61af74102b0131
sahelisom <57664811+sahelisom@users.noreply.github.com>
2019-11-20 17:00:14 +0530

    Add files via upload

---
+++ spring-cloud-config-server/src/test/java/com/payroll/microservices/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payroll.microservices.springcloudconfigserver;
+
+import org.junit.*;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
